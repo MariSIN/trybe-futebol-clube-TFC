e0b095a8ab4c3a25aefc6b20e0e91b85422bc60c
Mariana Nascimento <mariana.sn.silva@gmail.com>
2023-03-29 16:38:32 -0300

    [feat]: Cria migration e model para a tabela teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,27 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  timestamps: false,
+  tableName: 'teams',
+  underscored: true,
+});
+
+export default Teams;
